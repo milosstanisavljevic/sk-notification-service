465c546305b3ceac1e2a36dc9f06a514ad6e282c
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-12-23 17:42:46 +0100

    init

---
+++ src/main/java/com/raf/notifiationservice/NotifiationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.notifiationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifiationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifiationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raf/notifiationservice/NotifiationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.notifiationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifiationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
