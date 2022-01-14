2dc32aeaf85e7308f9a409339ce91efdd695645f
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2022-01-14 23:17:11 +0100

    final

--- src/main/java/com/raf/notifiationservice/scheduler/SimpleScheduler.java
+++ src/main/java/com/raf/notifiationservice/scheduler/SimpleScheduler.java
@@ -1,21 +1,21 @@
 package com.raf.notifiationservice.scheduler;
 
 import com.raf.notifiationservice.dto.NotificationCreateDto;
+import com.raf.notifiationservice.service.EmailService;
 import com.raf.notifiationservice.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleScheduler {
 
-    private NotificationService notificationService;
-
-    public SimpleScheduler(NotificationService notificationService) {
-        this.notificationService = notificationService;
-    }
+    @Autowired
+    EmailService emailService;
 
     @Scheduled(fixedDelay = 1000000000, initialDelay = 1000000000)
-    public void reservationReminder(NotificationCreateDto notificationCreateDto){
-        notificationService.sendNotification(notificationCreateDto);
+    public void reservationReminder(){
+        emailService.sendSimpleMessage("mstanic5819rn@raf.rs","subject","sk2");
+
     }
 }
